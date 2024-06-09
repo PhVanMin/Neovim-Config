668e5c50a5a12444d481c41bc3554c8b4fb4342c
PhVanMin <phamminh3107@gmail.com>
2024-06-09 17:20:53 +0700

    feat: custom floating window

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,45 +1,56 @@
 local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
-	lsp_zero.default_keymaps({buffer = bufnr})
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
+    ensure_installed = {},
+    handlers = {
+        function(server_name)
+            local config = require('lspconfig')
+            if server_name == 'jdtls' then
+                config[server_name].setup({
+                    root_dir = function(fname)
+                        local primary = config.util.root_pattern("Main.java")(fname)
+                        return primary or vim.fn.getcwd()
+                    end
+                })
+            else
+                config[server_name].setup({})
+            end
+        end,
+    },
 })
 
 local cmp = require('cmp')
 cmp.setup({
-  sources = {
-    {name = 'nvim_lsp'},
-  },
-  mapping = {
-    ['<C-y>'] = cmp.mapping.confirm({select = true}),
-    ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-    ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
-    ['<C-p>'] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_prev_item({behavior = 'insert'})
-      else
-        cmp.complete()
-      end
-    end),
-    ['<C-n>'] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_next_item({behavior = 'insert'})
-      else
-        cmp.complete()
-      end
-    end),
-  },
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
+    sources = {
+        { name = 'nvim_lsp' },
+    },
+    mapping = {
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Up>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+        ['<Down>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+        ['<C-p>'] = cmp.mapping(function()
+            if cmp.visible() then
+                cmp.select_prev_item({ behavior = 'insert' })
+            else
+                cmp.complete()
+            end
+        end),
+        ['<C-n>'] = cmp.mapping(function()
+            if cmp.visible() then
+                cmp.select_next_item({ behavior = 'insert' })
+            else
+                cmp.complete()
+            end
+        end),
+    },
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
 })
---
+++ after/plugin/my_plugin.lua
@@ -0,0 +1,51 @@
+local win;
+
+function OpenWin()
+    -- Create option variables
+    local winW = vim.api.nvim_win_get_width(0)
+    local winH = vim.api.nvim_win_get_height(0)
+    local bufW = math.floor(winW / 1.5)
+    local bufH = math.floor((winH - 2) / 1.5)
+    --[[ local bufW = 10
+    local bufH = 1 ]]
+    local title = "Input"
+
+    -- Create options
+    local opts = {
+        border = "rounded",
+        focusable = true,
+        relative = "editor",
+        width = bufW,
+        height = bufH,
+        row = math.floor((winH - bufH) / 2),
+        col = math.floor((winW - bufW) / 2),
+        style = 'minimal',
+        title = title,
+        title_pos = "center",
+    }
+
+    -- Show floating window
+    local buf = vim.api.nvim_create_buf(false, true)
+    win = vim.api.nvim_open_win(buf, 0, opts)
+
+    -- Create input field for User
+    vim.api.nvim_buf_set_text(buf, 0, 0, 0, 0, { "" })
+    vim.api.nvim_win_set_cursor(0, { 1, 1 });
+    vim.cmd("startinsert")
+
+    -- Listen for input
+    vim.keymap.set("i", "<CR>", function()
+        local line = vim.api.nvim_buf_get_lines(buf, 0, 1, false)
+        -- TODO process user input
+        vim.cmd("stopinsert")
+        vim.api.nvim_win_close(win, true)
+    end, { buffer = buf })
+
+    vim.keymap.set("n", "q", function()
+        vim.api.nvim_win_close(win, true)
+    end, { buffer = buf })
+end
+
+function CloseWin()
+    vim.api.nvim_win_close(win, true)
+end
